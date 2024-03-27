86a7246baf47913df0949f5d6b1ddc6ebea8ded2
Allison Tearjen <allison.tearjen@snyk.io>
2024-03-27 16:28:57 -0500

    making a change to make a new branch

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
 /*
  * Copyright (c) 2014-2022 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
+ * SPDX-License-Identifier: MIT 
  */
-
+//just adding this comment to have a change in a new branch for testing purposes
 import { enableProdMode } from '@angular/core'
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'
 
